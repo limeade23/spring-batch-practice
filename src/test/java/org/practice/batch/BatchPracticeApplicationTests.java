1cd032b914208a0b045f5b90453d4fd629cd2a2f
limeade23 <eriol72@limeade.me>
2025-08-03 23:32:42 +0900

    batch project first setting

---
+++ src/test/java/org/practice/batch/BatchPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.practice.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
