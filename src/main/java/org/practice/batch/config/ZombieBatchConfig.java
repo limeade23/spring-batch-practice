5edf13b9447ddb9b852c804028a3e87e95a23086
junhan.koo <junhan.koo@btsoft.kr>
2025-08-07 18:50:27 +0900

    dd

--- src/main/java/org/practice/batch/config/ZombieBatchConfig.java
+++ src/main/java/org/practice/batch/config/ZombieBatchConfig.java
@@ -1,15 +1,16 @@
 package org.practice.batch.config;
 
+import org.practice.batch.tasklet.ZombieProcessCleanupTasklet;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.support.transaction.ResourcelessTransactionManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.ResourceTransactionManager;
 
 @Configuration
 public class ZombieBatchConfig {
@@ -29,8 +30,11 @@ public class ZombieBatchConfig {
     @Bean
     public Step zombieCleanupStep() {
         return new StepBuilder("zombieCleanupStep", jobRepository)
-                // Tasklet과 ResourcelessTransactionManager 설정
-                .tasklet(zombieProcessCleanupTasklet(), new ResourcelessTransactionManager())
+                // Tasklet과 transactionManager 설정
+                // tasklet() 메서드를 호출하면, 스텝 빌더는 태스크릿 지향 처리 방식의 Step을 생성
+                .tasklet(zombieProcessCleanupTasklet(), transactionManager)
+                // DB 트랜잭션이 필요 없는 Tasklet일 경우 PlatformTransactionManager 대신 ResourcelessTransactionManager를 고려
+                // .tasklet(zombieProcessCleanupTasklet(), new ResourceTransactionManager())
                 .build();
     }
 
@@ -41,4 +45,3 @@ public class ZombieBatchConfig {
                 .build();
     }
 }
-
